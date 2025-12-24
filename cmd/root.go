0085a7eafef1cfd1bfb564ea90cce3109eff7ac0
george <gtechong72@gmail.com>
2025-12-24 21:22:30 +0300

    chore: add github actions for automated releases

--- cmd/root.go
+++ cmd/root.go
@@ -26,7 +26,7 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tidyup.yaml)")
-	rootCmd.Version = "0.3.0"
+	rootCmd.Version = "0.5.0"
 }
 
 func initConfig() {
